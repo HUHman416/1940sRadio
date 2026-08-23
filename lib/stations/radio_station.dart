b411e7b2db9a401da291bd724ef833fd99a48886
HUHman416 <michaelvandegrift1@gmail.com>
2026-08-22 22:45:01 -0700

    Add station model for Build 0.2

---
+++ lib/stations/radio_station.dart
@@ -0,0 +1,49 @@
+class RadioStation {
+  const RadioStation({
+    required this.id,
+    required this.name,
+    required this.url,
+    this.subtitle = '',
+    this.builtIn = false,
+  });
+
+  static const fogPoint = RadioStation(
+    id: 'fog-point-radio',
+    name: 'FOG POINT RADIO',
+    url: 'https://streaming.live365.com/a25002',
+    subtitle: 'DRIFT BAY BROADCASTING SERVICE',
+    builtIn: true,
+  );
+
+  final String id;
+  final String name;
+  final String url;
+  final String subtitle;
+  final bool builtIn;
+
+  RadioStation copyWith({String? name, String? url, String? subtitle}) {
+    return RadioStation(
+      id: id,
+      name: name ?? this.name,
+      url: url ?? this.url,
+      subtitle: subtitle ?? this.subtitle,
+      builtIn: builtIn,
+    );
+  }
+
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'name': name,
+        'url': url,
+        'subtitle': subtitle,
+      };
+
+  factory RadioStation.fromJson(Map<String, dynamic> json) {
+    return RadioStation(
+      id: json['id'] as String,
+      name: json['name'] as String,
+      url: json['url'] as String,
+      subtitle: (json['subtitle'] as String?) ?? '',
+    );
+  }
+}
